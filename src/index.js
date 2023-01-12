0272c3e2dae7ef09aabc3850b7dd0cc3ce2e7906
y8nju <89aruc@gmail.com>
2023-01-12 20:46:34 +0900

    First: Todo

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
-const { createStore } = require("redux");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD"
-const MINUS = "MINUS"
-
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  // eslint-disable-next-line default-case
-  switch(action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS: 
-      return count - 1;
-    default:
-      return count;
-  }
-}
-
-// store: 데이터 저장
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const createToDo = toDo => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-// subscribe: store 안에 있는 변화들을 알 수 있다
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({type: ADD});
-
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS});
-}
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
