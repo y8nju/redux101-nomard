3b5248fe5326fad32955fd0346e90b4a256b74d9
y8nju <89aruc@gmail.com>
2023-01-12 17:24:01 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  // count++;
-  // count--;
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1
+  } else if (action.type === "MINUS") {
+    return count - 1
+  } else {
+    return count;
+  }
+  // eslint-disable-next-line default-case
+  // switch(action.type) {
+  //   case "ADD":
+  //     return count + 1;
+  //   case "MINUS": 
+  //     return count - 1;
+  // }
+  // return count;
 }
 
+// store: 데이터 저장
 const countStore = createStore(countModifier);
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+console.log(countStore.getState());
