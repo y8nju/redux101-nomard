b6b7e9f12749260b94a5788c95579b9136735fd4
y8nju <89aruc@gmail.com>
2023-01-12 20:55:15 +0900

    #2.0 Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
+const { createStore } = require("redux");
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = toDo => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
-};
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// const createToDo = toDo => {
+//   const li = document.createElement("li");
+//   li.innerText = toDo;
+//   ul.appendChild(li);
+// };
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({type: ADD_TODO, text: toDo});
 };
 
 form.addEventListener("submit", onSubmit);
