1119c3d6a87ad35b87346a390426a3d17cc7619f
y8nju <89aruc@gmail.com>
2023-01-12 17:07:43 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
+const { createStore } = require("redux");
+
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+const countModifier = (count = 0) => {
+  // count++;
+  // count--;
+  return count;
+}
 
-// number를 업데이트하는 함수
-const updateText = () => {
-  number.innerText = count;
-};
-const handleAdd = () => {
-  // count = count + 1;
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  // count = count - 1;
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
